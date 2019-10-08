a25b953c93b302336559eab535a1cbecd3aff38c
Lucas Robitaille <lrjames237@gmail.com>
2019-10-08 15:32:07 -0500

    prepared for deploy

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,17 @@
 
 export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id == folderId)
+  folders.find(folder => folder.id === folderId)
 
 export const findNote = (notes=[], note_id) =>
+// eslint-disable-next-line
   notes.find(note => note.id == note_id)
 
 export const getNotesForFolder = (notes=[], folderId) => (
   (!folderId)
     ? notes
+    // eslint-disable-next-line
     : notes.filter(note => note.folder_id == folderId)
 )
 
 export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folder_id == folderId).length
+  notes.filter(note => note.folder_id === folderId).length
